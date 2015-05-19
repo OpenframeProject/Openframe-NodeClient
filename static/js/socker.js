7cf954a86ea09096d5fc89d3132d73ea12e96ea5
Jonathan Wohl <jon@jonwohl.com>
2015-05-18 22:49:22 -0400

    updated to match latest server code

---
+++ static/js/socker.js
@@ -0,0 +1,114 @@
+Socker = (function() {
+    var _self = {},
+        _eventHandlers = {},
+        _connected = false,
+        _opts = {
+            keepAlive: true,
+            checkInterval: 10000
+        },
+        _url,
+        _ws;
+
+    function _connect(url, opts) {
+        _url = url;
+        if (opts) _extend(_opts, opts);
+        _ws = new WebSocket(url);
+
+        _ws.onopen = function() {
+            console.log('connection opened');
+            _connected = true;
+            if (_opts.onOpen) _opts.onOpen();
+        };
+
+        _ws.onclose = function() {
+            console.log('connection closed');
+            _connected = false;
+            if (_opts.onClose) _opts.onClose();
+        };
+
+        _ws.onmessage = function(evt) {
+            var message = JSON.parse(evt.data),
+                name = message.name,
+                data = message.data;
+
+            console.log(message);
+
+            if (_eventHandlers[name]) {
+                // for each event handler, call the callback
+                for (var i = 0; i < _eventHandlers[name].length; i++) {
+                    _eventHandlers[name][i](data);
+                }
+            } else {
+                console.log(name + " event not handled.");
+            }
+        };
+
+        if (_opts.keepAlive) {
+            setInterval(_checkConnection, _opts.checkInterval);
+        }
+    }
+
+    /**
+     * Add event handler
+     * @param  {[type]}   name     [description]
+     * @param  {Function} callback [description]
+     * @return {[type]}            [description]
+     */
+    function _on(name, callback) {
+        if (_eventHandlers[name]) {
+            _eventHandlers[name].push(callback);
+        } else {
+            _eventHandlers[name] = [callback];
+        }
+    }
+
+    /**
+     * Remove event handler
+     * @param  {[type]}   name     [description]
+     * @param  {Function} callback [description]
+     * @return {[type]}            [description]
+     */
+    function _off(name, callback) {
+        if (_eventHandlers[name]) {
+            var index = _eventHandlers[name].indexOf(callback);
+            if (index > -1) {
+                _eventHandlers[name].splice(index, 1);
+            }
+        } else {
+            // do nothing
+        }
+    }
+
+    function _send(name, data) {
+        message = {
+            name: name,
+            data: data
+        };
+
+        _ws.send(JSON.stringify(message));
+    }
+
+    function _checkConnection() {
+        if (!_connected) {
+            _connect(_url, _opts);
+        }
+    }
+
+    function _extend(obj) {
+        Array.prototype.slice.call(arguments, 1).forEach(function(source) {
+            if (source) {
+                for (var prop in source) {
+                    obj[prop] = source[prop];
+                }
+            }
+        });
+        return obj;
+    }
+
+
+    _self.on = _on;
+    _self.off = _off;
+    _self.send = _send;
+    _self.connect = _connect;
+    return _self;
+})();
